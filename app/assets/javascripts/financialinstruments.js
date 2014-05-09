99a14e5f343e212b73e5f380e4e127602b3ac98f
RahulKhanna <sociallyrk@gmail.com>
2014-05-09 16:58:45 -0700

    consolidated dropdown

--- app/assets/javascripts/financialinstruments.js
+++ app/assets/javascripts/financialinstruments.js
@@ -1,40 +1,7 @@
 		  
 $(document).ready( function () {
 		  $("#instrument_sub").hide();
-		  $("#asset select").hide();
-  		$("#liability select").hide();
-  		$("#income select").hide();
-  		$("#expense select").hide();
-
   $("#financialinstrument_instrument_type").change(function() {
-  	  if ($(this).val() === "liability") {
   	  $("#instrument_sub").show();
-  	 	$("#liability select").show();
-  	 	$("#asset select").hide();
-  		$("#income select").hide();
-  		$("#expense select").hide();
-  		}
-  		if ($(this).val() === "asset") {
-  		$("#instrument_sub").show();
-  		$("#liability select").hide();
-  		$("#income select").hide();
-  		$("#expense select").hide();
-  	 	$("#asset select").show();
-  		}
-  		if ($(this).val() === "income") {
-  		$("#instrument_sub").show();
-		  $("#asset select").hide();
-  		$("#liability select").hide();
-  		$("#income select").show();
-  		$("#expense select").hide();
-  		}
-  		if ($(this).val() === "expense") {
-		  $("#instrument_sub").show();
-		  $("#asset select").hide();
-  		$("#liability select").hide();
-  		$("#income select").hide();
-  	 	$("#expense select").show();
-  		}
-  });
-  }
-);
+    }
+};
